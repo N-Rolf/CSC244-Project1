55f36948ad28e18ff3cd6bbffd1748337e8b8297
N-Rolf <neilrolf24@gmail.com>
2022-10-13 10:34:30 -0500

    nextState debug

--- Quartus Files/encoder_42.sv
+++ Quartus Files/encoder_42.sv
@@ -3,7 +3,7 @@ module encoder_42 (
 	output logic enc_out0, enc_out1
 );
 	
-	and (enc_out0, enc1, enc3);
-	and (enc_out1, enc2, enc3);
+	or (enc_out0, enc1, enc3);
+	or (enc_out1, enc2, enc3);
 	
 endmodule
--- Quartus Files/statetoHex.sv
+++ Quartus Files/statetoHex.sv
@@ -8,40 +8,34 @@ module statetoHex (
 				 hex0_0, hex0_1, hex0_2, hex0_3
 );
 
+	assign hex5_3 = 0;
 	assign hex5_0 = 0;
 	assign hex5_1 = 0;
-	assign hex5_2 = 0;
-	or (hex5_3, stateIn_4, stateIn_5, stateIn_6); 
-	//sev_seg hex5 (.hexIn_0(hex5_0), .hexIn_1(hex5_1), .hexIn_2(hex5_2), .hexIn_3(hex5_3));
+	or (hex5_2, stateIn_4, stateIn_5, stateIn_6); //the hex order here is cursed, don't touch
 	
-	assign hex4_0 = 0;
-	or (hex4_1, stateIn_2, stateIn_3, stateIn_6); 
-	or (hex4_2, stateIn_1, stateIn_3, stateIn_5);
-	or (hex4_3, stateIn_2, stateIn_3, stateIn_6); 
-	//sev_seg hex4 (.hexIn_0(hex4_0), .hexIn_1(hex4_1), .hexIn_2(hex4_2), .hexIn_3(hex4_3));
+	assign hex4_3 = 0;
+	or (hex4_2, stateIn_2, stateIn_3, stateIn_6); 
+	or (hex4_1, stateIn_1, stateIn_3, stateIn_5);
+	or (hex4_0, stateIn_2, stateIn_3, stateIn_6); 
 
-	assign hex3_0 = 0;
-	or (hex3_1, stateIn_1, stateIn_3, stateIn_5); 
-	assign hex3_2 = 0;
-	or (hex3_3, stateIn_1, stateIn_3, stateIn_5); 
-	//sev_seg hex3 (.hexIn_0(hex3_0), .hexIn_1(hex3_1), .hexIn_2(hex3_2), .hexIn_3(hex3_3));
+	assign hex3_3 = 0;
+	or (hex3_2, stateIn_1, stateIn_3, stateIn_5); 
+	assign hex3_1 = 0;
+	or (hex3_0, stateIn_1, stateIn_3, stateIn_5); 
 
-	assign hex2_0 = 0;
-	assign hex2_1 = 0;
-	assign hex2_2 = 0;
 	assign hex2_3 = 0;
-	//sev_seg hex2 (.hexIn_0(hex2_0), .hexIn_1(hex2_1), .hexIn_2(hex2_2), .hexIn_3(hex2_3));
+	assign hex2_2 = 0;
+	assign hex2_1 = 0;
+	assign hex2_0 = 0;
 
-	assign hex1_0 = 0;
-	or (hex1_1, stateIn_5, stateIn_6);
-	or (hex1_2, stateIn_4, stateIn_6);
-	or (hex1_3, stateIn_5, stateIn_6);
-	//sev_seg hex1 (.hexIn_0(hex1_0), .hexIn_1(hex1_1), .hexIn_2(hex1_2), .hexIn_3(hex1_3));
+	assign hex1_3 = 0;
+	or (hex1_2, stateIn_5, stateIn_6);
+	or (hex1_1, stateIn_4, stateIn_6);
+	or (hex1_0, stateIn_5, stateIn_6);
 	
-	assign hex0_0 = 0;
-	or (hex0_1, stateIn_4, stateIn_6);
-	assign hex0_2 = 0;
-	or (hex0_3, stateIn_4, stateIn_6);
-	//sev_seg hex0 (.hexIn_0(hex0_0), .hexIn_1(hex0_1), .hexIn_2(hex0_2), .hexIn_3(hex0_3));
+	assign hex0_3 = 0;
+	or (hex0_2, stateIn_4, stateIn_6);
+	assign hex0_1 = 0;
+	or (hex0_0, stateIn_4, stateIn_6);
 
 endmodule
