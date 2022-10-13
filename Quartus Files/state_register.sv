3e89217075e0a9fab977095a8d3dda9ee9c20690
N-Rolf <neilrolf24@gmail.com>
2022-10-13 05:58:26 -0500

    hex, hex logic, register init

--- Quartus Files/state_register.sv
+++ Quartus Files/state_register.sv
@@ -1,22 +1,22 @@
 module state_register (
-	input logic CLK, D0, D1, D2, D3, D4, D5, D6,
-	output logic Q0, Q1, Q2, Q3, Q4, Q5, Q6
+	input logic CLK, data0, data1, data2, data3, data4, data5, data6,
+	output logic state0, state1, state2, state3, state4, state5, state6
 );
 	
 	//S0 - $0.00
-	d_ff S0 (.dff_D(D0), .dff_CLK(CLK), .dff_Q(Q0));
+	d_ff S0 (.dff_D(data0), .dff_CLK(CLK), .dff_Q(state0));
 	//S1 - $0.25
-	d_ff S1 (.dff_D(D1), .dff_CLK(CLK), .dff_Q(Q1));
+	d_ff S1 (.dff_D(data1), .dff_CLK(CLK), .dff_Q(state1));
 	//S2 - $0.50
-	d_ff S2 (.dff_D(D2), .dff_CLK(CLK), .dff_Q(Q2));
+	d_ff S2 (.dff_D(data2), .dff_CLK(CLK), .dff_Q(state2));
 	//S3 - $0.75
-	d_ff S3 (.dff_D(D3), .dff_CLK(CLK), .dff_Q(Q3));
+	d_ff S3 (.dff_D(data3), .dff_CLK(CLK), .dff_Q(state3));
 	//S4 - $1.00
-	d_ff S4 (.dff_D(D4), .dff_CLK(CLK), .dff_Q(Q4));
+	d_ff S4 (.dff_D(data4), .dff_CLK(CLK), .dff_Q(state4));
 	//S5 - $1.25
-	d_ff S5 (.dff_D(D5), .dff_CLK(CLK), .dff_Q(Q5));
+	d_ff S5 (.dff_D(data5), .dff_CLK(CLK), .dff_Q(state5));
 	//S6 - $1.50
-	d_ff S6 (.dff_D(D6), .dff_CLK(CLK), .dff_Q(Q6));
+	d_ff S6 (.dff_D(data6), .dff_CLK(CLK), .dff_Q(state6));
 	
 	
 endmodule
