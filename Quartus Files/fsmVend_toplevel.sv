2e25be387b2f6695d658bdd0e124db6c6564a5bd
N-Rolf <neilrolf24@gmail.com>
2022-10-13 07:55:35 -0500

    next state init

--- Quartus Files/fsmVend_toplevel.sv
+++ Quartus Files/fsmVend_toplevel.sv
@@ -6,36 +6,23 @@ module fsmVend_toplevel(
 	logic CLK_clean, change, encoded_input0, encoded_input1, dec_NS[0:8];
 	debouncer clean (.A_noisy(key_CLK), .CLK50M(CLK), .A(CLK_clean));
 	
-	//inputs:
-	//dollar, halfDollar, quarter SW[2:0]
-	//CLK KEY0
-	//RES KEY1
-	//Current State Output LEDR[S-1:0]
-	//guffin output LEDR[9]
-	//halfDollar output LEDR[8]
-	//quarter output LEDR[7]
-	//state output HEX3-5
-	//change output HEX0-2
-	
 	//input -> 4:2 encoder
-	//encoder_42 encode (.enc1(quarter_in), .enc2(halfDollar_in), .enc3(dollar_in), .enc_out0(encoded_input0), .enc_out1(encoded_input1));
+	encoder_42 encode (.enc1(quarter_in), .enc2(halfDollar_in), .enc3(dollar_in), .enc_out0(encoded_input0), .enc_out1(encoded_input1));
+	
 	//4:2 encoded -> nextState Logic
+	next_state nxtst (.encInput_X(enc1), .encInput_Y(enc2), .currState_0(encState_0), currState_1(encState_1), currState_2(encState_2));
 	
 	//nextState Logic -> 4:8 decoder
-	//decoder_48 decode (.dec0(), .dec1(), .dec2(), .dec3(), .dec4(), .dec_out0(dec_NS[0]), .dec_out1(dec_NS[1]), .dec_out2(dec_NS[2]), .dec_out3(dec_NS[3]), .dec_out4(dec_NS[4]), .dec_out5(dec_NS[5]), .dec_out6(dec_NS[6]));
+	decoder_48 decode (.dec0(nextState_0), .dec1(nextState_1), .dec2(nextState_2), .dec3(nextState_3), .dec_out0(dec_NS[0]), .dec_out1(dec_NS[1]), .dec_out2(dec_NS[2]), .dec_out3(dec_NS[3]), .dec_out4(dec_NS[4]), .dec_out5(dec_NS[5]), .dec_out6(dec_NS[6]));
 	
 	//4:8 decoder -> register
-	//state_register state_reg (.CLK(CLK_clean), .D0(dec_NS[0]), .D1(dec_NS[1]), .D2(dec_NS[2]), .D3(dec_NS[3]), .D4(dec_NS[4]), .D5(dec_NS[5]), .D6(dec_NS[6]));
+	state_register state_reg (.CLK(CLK_clean), .D0(dec_NS[0]), .D1(dec_NS[1]), .D2(dec_NS[2]), .D3(dec_NS[3]), .D4(dec_NS[4]), .D5(dec_NS[5]), .D6(dec_NS[6]));
 	
 	//register -> output logic
 	//guffinOut_logic output ();
-	//register -> HEX logic STATE
-	//statetoHex stateHex ();
-	//register -> HEX logic CHANGE
-	//changetoHex changeHex();
-	//HEX logic -> 7 segment STATE
-	//sev_seg dispState ();
-	//HEX logic -> 7 segment CHANGE
-	//sev_seg dispChange ();
+
+	//register -> HEX logic STATE & CHANGE
+		//this function calls sev_seg internally
+	statetoHex stateHex (.stateIn_0(state0), .stateIn_1(state1), .stateIn_2(state2), .stateIn_3(state3), .stateIn_4(state4), .stateIn_5(state5), .stateIn_6(state6));
 	
 endmodule
--- Quartus Files/statetoHex.sv
+++ Quartus Files/statetoHex.sv
@@ -1,5 +1,5 @@
 module statetoHex (
-	input logic state0, state1, state2, state3, state4, state5, state6,
+	input logic stateIn_0, stateIn_1, stateIn_2, stateIn_3, stateIn_4, stateIn_5, stateIn_6,
 	output logic 
 );
 	logic hex5_0, hex5_1, hex5_2, hex5_3,
@@ -13,19 +13,19 @@ module statetoHex (
 	hex5_0 = 0;
 	hex5_1 = 0;
 	hex5_2 = 0;
-	or (hex5_3, state4, state5, state6); 
+	or (hex5_3, stateIn_4, stateIn_5, stateIn_6); 
 	sev_seg hex5 (.hexIn_0(hex5_0), .hexIn_1(hex5_1), .hexIn_2(hex5_2), .hexIn_3(hex5_3));
 	
 	hex4_0 = 0;
-	or (hex4_1, state2, state3, state6); 
-	or (hex4_2, state1, state3, state5);
-	or (hex4_3, state2, state3, state6); 
+	or (hex4_1, stateIn_2, stateIn_3, stateIn_6); 
+	or (hex4_2, stateIn_1, stateIn_3, stateIn_5);
+	or (hex4_3, stateIn_2, stateIn_3, stateIn_6); 
 	sev_seg hex4 (.hexIn_0(hex4_0), .hexIn_1(hex4_1), .hexIn_2(hex4_2), .hexIn_3(hex4_3));
 
 	hex3_0 = 0;
-	or (hex3_1, state1, state3, state5); 
+	or (hex3_1, stateIn_1, stateIn_3, stateIn_5); 
 	hex3_2 = 0;
-	or (hex3_3, state1, state3, state5); 
+	or (hex3_3, stateIn_1, stateIn_3, stateIn_5); 
 	sev_seg hex3 (.hexIn_0(hex3_0), .hexIn_1(hex3_1), .hexIn_2(hex3_2), .hexIn_3(hex3_3));
 
 	hex2_0 = 0;
@@ -35,15 +35,15 @@ module statetoHex (
 	sev_seg hex2 (.hexIn_0(hex2_0), .hexIn_1(hex2_1), .hexIn_2(hex2_2), .hexIn_3(hex2_3));
 
 	hex1_0 = 0;
-	or (hex1_1, state5, state6);
-	or (hex1_2, state4, state6);
-	or (hex1_3, state5, state6);
+	or (hex1_1, stateIn_5, stateIn_6);
+	or (hex1_2, stateIn_4, stateIn_6);
+	or (hex1_3, stateIn_5, stateIn_6);
 	sev_seg hex1 (.hexIn_0(hex1_0), .hexIn_1(hex1_1), .hexIn_2(hex1_2), .hexIn_3(hex1_3));
 	
 	hex0_0 = 0;
-	or (hex0_1, state4, state6);
+	or (hex0_1, stateIn_4, stateIn_6);
 	hex0_2 = 0;
-	or (hex0_3, state4, state6);
+	or (hex0_3, stateIn_4, stateIn_6);
 	sev_seg hex0 (.hexIn_0(hex0_0), .hexIn_1(hex0_1), .hexIn_2(hex0_2), .hexIn_3(hex0_3));
 
 endmodule
